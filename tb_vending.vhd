45ab86ed72c312ce381fd67855e3b75e48ee6022
acontius <152691439+acontius@users.noreply.github.com>
2024-02-19 22:27:16 +0330

    fixing some bug

--- tb_vending.vhd
+++ tb_vending.vhd
@@ -4,16 +4,27 @@ use ieee.std_logic_1164.all;
 entity tb_vending is
 end entity;
 
+
 architecture Behavior of tb_vending is 
+
+    component vending is
+        port
+        (   Coin_in clk           : in  std_logic;
+            T15_out,T4_out,T5_out : in  std_logic;
+            Submit                : in  std_logic;
+            ticket                : out std_logic;
+            value                 : integer std_logic_vector(5 downto 0)) ;         
+    end component;
+
     signal    clk                 : std_logic;
     signal    Coin_in             : std_logic_vector(2 downto 0);
     signal    ticket              : std_logic_vector(2 downto 0);
     signal    Submit              : std_logic;
     signal    T5_out,T4_out,T15_out,Remaining_money : std_logic;
     signal    value               :integer range 0 to 100000;
-begin
 
-utt:Vending port map(
+begin
+    uut:Vending port map(
     clk => clk,
     RST => RST,
     Coin_in => Coin_in,
@@ -25,7 +36,6 @@ utt:Vending port map(
     Remaining_money => Remaining_money,
     value => value
 );
-
 clk <= not clk after 4 ns;
 
 process
