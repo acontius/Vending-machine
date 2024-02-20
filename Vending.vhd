380bf6a82c9bd044921cd9634c76d37025f7bc25
acontius <152691439+acontius@users.noreply.github.com>
2024-02-20 12:39:21 +0330

    fixing some bug

--- Vending.vhd
+++ Vending.vhd
@@ -1,28 +1,30 @@
 library ieee;
-use ieee.numeric_std.all
+use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 
-entity Vending is 
+entity vending is 
     port (
         clk                 : in std_logic;
-        Coin_in             : in std_logic_vector(2 downto 0);
-        ticket              : in std_logic_vector(2 downto 0);
+        Coin_in             : in std_logic_vector(1 downto 0);
+        ticket              : in std_logic_vector(1 downto 0);
         Submit              : in std_logic;
         T5_out,T4_out,T15_out,Remaining_money : out std_logic;
         value               :out integer range 0 to 100000
     );
-end entity Vending;
-architecture Behavior of Vending is
-signal currentMoney : integer range 0 to 100000 := 0;
+end entity vending;
+
+architecture Behavior of vending is
+
+signal temp : integer range 0 to 100000 := 0;
 
 begin 
     
     process (clk)
     begin 
         T5_out <= '0' ;
-        T4_out <= '0'';
+        T4_out <= '0';
         T15_out <= '0' ;
-        value <= '0';
+        value <= 0;
         if ticket = "00" then
             if temp >= 500 then 
                 temp <= temp - 500;
@@ -39,12 +41,12 @@ begin
                 T4_out <= '1';
             end if;
         elsif Coin_in = "01" then 
-            currentMoney <= currentMoney + 500;
+            temp <= temp + 500;
         elsif Coin_in = "10" then 
-            currentMoney <= currentMoney + 1000;
+            temp <= temp + 1000;
         elsif Submit = '1' then 
             value <= temp;
-            temp <= '0';
+            temp <= 0;
         end if;
     end process;
 end architecture;
--- tb_vending.vhd
+++ tb_vending.vhd
@@ -1,25 +1,26 @@
 library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
+
 entity tb_vending is
 end entity;
 
 
-architecture Behavior of tb_vending is 
-
-    component Vending is
-        port
-        (   Coin_in clk           : in  std_logic;
-            T15_out,T4_out,T5_out : in  std_logic;
-            Submit                : in  std_logic;
-            ticket                : out std_logic;
-            value                 : out integer range 0 to 100000;    
-        );
+architecture Behavior of tb_vending is  
+    component vending is
+        port (
+            clk                 : in std_logic;
+            Coin_in             : in std_logic_vector(1 downto 0);
+            ticket              : in std_logic_vector(1 downto 0);
+            Submit              : in std_logic;
+            T5_out,T4_out,T15_out,Remaining_money : out std_logic;
+            value               :out integer range 0 to 100000
+        );      
     end component;
 
-    signal    clk                 : std_logic;
-    signal    Coin_in             : std_logic_vector(2 downto 0);
-    signal    ticket              : std_logic_vector(2 downto 0);
+    signal    clk                 : std_logic := '0';
+    signal    Coin_in             : std_logic_vector(1 downto 0);
+    signal    ticket              : std_logic_vector(1 downto 0);
     signal    Submit              : std_logic;
     signal    T5_out,T4_out,T15_out,Remaining_money : std_logic;
     signal    value               :integer range 0 to 100000;
@@ -27,7 +28,6 @@ architecture Behavior of tb_vending is
 begin
     uut:Vending port map(
     clk => clk,
-    RST => RST,
     Coin_in => Coin_in,
     ticket => ticket,
     Submit => Submit,
@@ -37,7 +37,7 @@ begin
     Remaining_money => Remaining_money,
     value => value
 );
-clk <= not clk after 4 ns;
+clk <= not clk after 20 ns;
 
 process
 begin
